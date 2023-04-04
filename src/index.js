f0eb7b85b2aac8c831cdaec323daa04a1d4ebcc0
Andraad3 <joao.andraade25@gmail.com>
2023-04-04 00:27:14 -0300

    create documentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger.json')
 
 const app = express()
 
 routes(app)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
